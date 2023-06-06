495d5309cf9d593b1843c17a0afd69b932da2013
Antônio Júnior <antonio.junior@prodepa.pa.gov.br>
2023-06-06 18:01:34 -0300

    test

--- src/main/java/org/camunda/bpm/getstarted/loanapproval/TramitacaoService.java
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/TramitacaoService.java
@@ -1,11 +1,19 @@
 package org.camunda.bpm.getstarted.loanapproval;
 
-import org.springframework.stereotype.Component;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-@Component
-public class TramitacaoService {
-    
-    public void enviarParaAutorizador(){
+
+public class TramitacaoService implements JavaDelegate {
+
+
+    @Override
+    public void execute(DelegateExecution de) throws Exception {
+        
         System.out.println("Enviando para autorizador");
+
+        de.setVariable("localizacaoAtualSigla", "DDS");
+        de.setVariable("localizacaoAtualNome", "Diretoria de Desenvolvimento de Sistemas");
+        
     }
 }
